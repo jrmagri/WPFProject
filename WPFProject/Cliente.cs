10d308a3ec6a1bd3d06845217cb9323cda5c000e
juniormagri <juniormagrix@gmail.com>
2023-03-20 12:35:08 -0300

    Adicionar arquivos de projeto.

---
+++ WPFProject/Cliente.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFProject
+{
+    public class Cliente : INotifyPropertyChanged
+    {
+        public string primeiroNome;
+        public string sobreNome;
+        public string telefone;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string PrimeiroNome
+        {
+            get => primeiroNome;
+            set
+            {
+                if(primeiroNome != value)
+                {
+                    primeiroNome = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        public string SobreNome
+        {
+            get => sobreNome;
+            set
+            {
+                if (sobreNome != value)
+                {
+                    sobreNome = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        public string Telefone
+        {
+            get => telefone;
+            set
+            {
+                if (telefone != value)
+                {
+                    telefone = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public void NotifyPropertyChanged([CallerMemberName] string propName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+
+
+
+    }
+}
---
+++ WPFProject/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Cliente> Clientes { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Clientes = new ObservableCollection<Cliente>();
+            Clientes.Add(new Cliente { PrimeiroNome = "Maria", SobreNome = "Silva", Telefone = "16-98834-9098" });
+            Clientes.Add(new Cliente { PrimeiroNome = "Rodolfo", SobreNome = "Peratello", Telefone = "16-7788-3477" });
+            Clientes.Add(new Cliente { PrimeiroNome = "Bianca", SobreNome = "Rohrer", Telefone = "16-98823-3322" });
+            Clientes.Add(new Cliente { PrimeiroNome = "Matheus", SobreNome = "Rodrigues", Telefone = "16-99934-9196" });
+
+        }
+
+       
+    }
+}
